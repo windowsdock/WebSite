77cfa0b9ab7526fc1a6b42ae11a371a40dc8aacb
Nikola Hristov <nikola@playform.cloud>
2022-01-18 16:46:14 +0200

    Cleanup

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
 module.exports = {
 	plugins: [
-		require("postcss-import")(),
-		require("postcss-url")(),
+		require("postcss-import"),
+		require("postcss-url"),
 		require("tailwindcss")("./tailwind.config.js"),
-		require("postcss-combine-media-query")(),
+		require("postcss-combine-media-query"),
 		require("postcss-combine-duplicated-selectors")({
 			removeDuplicatedProperties: true,
 			removeDuplicatedValues: false,
