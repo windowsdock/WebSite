eda401e7c85d7220648e5a629a8cb5f3caa7f0c6
Nikola Hristov <nikola@playform.cloud>
2022-01-26 02:21:14 +0200

    Removes unnecessary function calls

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,10 @@
 module.exports = {
 	plugins: [
-		require("postcss-import")(),
-		require("postcss-url")(),
+		require("postcss-import"),
+		require("postcss-url"),
 		require("tailwindcss")("./tailwind.config.js"),
 		require("autoprefixer"),
-		require("postcss-combine-media-query")(),
+		require("postcss-combine-media-query"),
 		require("postcss-combine-duplicated-selectors")({
 			removeDuplicatedProperties: true,
 			removeDuplicatedValues: false,
