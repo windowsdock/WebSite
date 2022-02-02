28163689691d73e4806b2970f8356a06be675e6a
Nikola Hristov <nikola@playform.cloud>
2022-02-02 02:55:26 +0200

    Adds @tailwindcss/line-clamp, @tailwindcss/nesting

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
 	plugins: [
 		require("postcss-import"),
 		require("postcss-url"),
+		require("@tailwindcss/nesting"),
 		require("tailwindcss")("./tailwind.config.js"),
 		require("postcss-combine-media-query"),
 		require("postcss-combine-duplicated-selectors")({
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
 	plugins: [
 		require("@tailwindcss/forms"),
 		require("@tailwindcss/typography"),
+		require("@tailwindcss/line-clamp"),
 		require("@tailwindcss/aspect-ratio"),
 	],
 };
