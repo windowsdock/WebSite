09f96b64077ef07e0422558715e57385b1888b34
Nikola Hristov <nikola@playform.cloud>
2022-02-13 16:44:01 +0200

    Bumps tailwindcss to current latest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,6 @@
 export default {
-	projectRoot: ".",
-	pages: "./src/pages",
-	dist: "./dist",
-	public: "./public",
 	buildOptions: {
 		// TODO Place your site URL here
 		// site: "",
-		sitemap: true,
 	},
-	devOptions: {
-		port: 3000,
-	},
-	renderers: [],
 };
