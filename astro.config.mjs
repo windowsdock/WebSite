f3dba59edf6dd70021a50474817dba1f2e80f2fe
Nikola Hristov <nikola@playform.cloud>
2022-04-18 03:10:45 +0300

    Adds turbolinks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
+import turbolinks from "@astrojs/turbolinks";
 
 export default defineConfig({
 	// TODO Place your site URL here
 	// site: "",
+	integrations: [turbolinks()],
+	trailingSlash: "always",
 });
