b1e22d97afd8b826d0ff7f1e9711ebf4be623246
Nikola Hristov <nikola@playform.cloud>
2022-05-08 05:31:54 +0300

    safe commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 import { defineConfig } from "astro/config";
 import critters from "astro-critters";
+import compress from "astro-compress";
 
 export default defineConfig({
 	// TODO Place your site URL here
 	// site: "",
+	experimental: {
+		integrations: true,
+	},
 	integrations: [
 		critters({
 			preload: "body",
 			inlineFonts: true,
 		}),
+		compress(),
 	],
 });
