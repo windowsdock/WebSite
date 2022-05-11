e677883718b060e1704a2f504e8a3a6a0cbc4286
Nikola Hristov <nikola@playform.cloud>
2022-05-12 02:59:54 +0300

    cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import compress from "astro-compress";
 
 export default defineConfig({
 	site: "https://windowsdock.app",
+	experimental: {
+		integrations: true,
+	},
 	integrations: [
 		critters({
 			preload: "body",
