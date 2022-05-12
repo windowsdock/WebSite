701975b6e7b3986ca8c861b3e95468391c400a10
Nikola Hristov <nikola@playform.cloud>
2022-05-12 03:00:32 +0300

    cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import compress from "astro-compress";
 export default defineConfig({
 	// TODO Place your site URL here
 	// site: "",
+	experimental: {
+		integrations: true,
+	},
 	integrations: [
 		critters({
 			preload: "body",
