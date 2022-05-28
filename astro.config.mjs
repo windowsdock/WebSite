fb7c37cee0bd3965cbbe5e9f848ac2ce463b152c
Nikola Hristov <nikola@playform.cloud>
2022-05-28 16:42:45 +0300

    Clear dist

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,7 @@ import critters from "astro-critters";
 import compress from "astro-compress";
 
 export default defineConfig({
-<<<<<<< HEAD
 	site: "https://windowsdock.app",
-=======
-	// TODO Place your site URL here
-	// site: "",
->>>>>>> upstream/main
 	experimental: {
 		integrations: true,
 	},
