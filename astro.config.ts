e1019ea9094396bb9b84d0c56f31d3c65c688a4b
Nikola Hristov <nikola@playform.cloud>
2022-07-10 09:57:54 +0300

    Cleanup

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "astro/config";
-import critters from "astro-critters";
-import compress from "astro-compress";
 import sitemap from "@astrojs/sitemap";
+import compress from "astro-compress";
+import critters from "astro-critters";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
 	site: "https://windowsdock.app",
