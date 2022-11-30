4a568cea9c98c2a35e9f526f14aaf472c2bafd68
Nikola Hristov <nikola@nikolahristov.tech>
2022-11-30 19:52:09 +0200

    squash!

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,12 @@
+import { defineConfig } from "astro/config";
+
 import compress from "astro-compress";
 import critters from "astro-critters";
-
+import prefetch from "@astrojs/prefetch";
+import rome from "astro-rome";
 import sitemap from "@astrojs/sitemap";
 
-/* A function that returns an object. */
-export default {
+export default defineConfig({
 	site: "https://windowsdock.app",
-	integrations: [
-		sitemap(),
-		critters(),
-		compress(),
-	],
-};
+	integrations: [sitemap(), critters(), prefetch(), rome(), compress()],
+});
