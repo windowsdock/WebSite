ea88f34406957522d1eb338c32d34d71b97d9cb2
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-22 05:06:24 +0200

    squash!

--- src/scripts/firebase.ts
+++ src/scripts/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyCHqHJ_lLPG3N1vPQOvuLDqQSyu0UlNRHs",
-	appId: "1:869935157727:web:38a6c7f9a49f7f7c30b46a",
 	authDomain: "windowsdock.firebaseapp.com",
 	databaseURL: "https://windowsdock.firebaseio.com",
-	measurementId: "G-MN7TLVEMR9",
-	messagingSenderId: "869935157727",
 	projectId: "windowsdock",
 	storageBucket: "windowsdock.appspot.com",
+	messagingSenderId: "869935157727",
+	appId: "1:869935157727:web:38a6c7f9a49f7f7c30b46a",
+	measurementId: "G-Q7BSEBHZ0Y"
 };
 
 initializeApp(firebaseConfig);
