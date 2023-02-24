c4bbfd3c45ba6df9fcb7c10a60c96daa86381305
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-24 04:06:17 +0200

    Prettifies everything

--- src/scripts/firebase.ts
+++ src/scripts/firebase.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
 	storageBucket: "windowsdock.appspot.com",
 	messagingSenderId: "869935157727",
 	appId: "1:869935157727:web:38a6c7f9a49f7f7c30b46a",
-	measurementId: "G-Q7BSEBHZ0Y"
+	measurementId: "G-Q7BSEBHZ0Y",
 };
 
 initializeApp(firebaseConfig);
