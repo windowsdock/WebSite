df55d22bc7910d00164e1c279e34818ad6652a41
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-04 19:19:35 +0300

    squash!

--- astro.config.ts
+++ astro.config.ts
@@ -8,6 +8,7 @@ import worker from "astrojs-service-worker";
 
 export default defineConfig({
 	srcDir: "./Source",
+	publicDir: "./Public",
 	site: "https://windowsdock.app",
 	experimental: {
 		assets: true,
