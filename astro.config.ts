34180a4e06a101789dd054619d345a1cc639f82e
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-31 12:57:43 +0300

    squash!

--- astro.config.ts
+++ astro.config.ts
@@ -11,10 +11,6 @@ export default defineConfig({
 	publicDir: "./Public",
 	outDir: "./Target",
 	site: "https://windowsdock.app",
-	experimental: {
-		assets: true,
-		viewTransitions: true,
-	},
 	compressHTML: true,
 	integrations: [
 		import.meta.env.MODE === "production" ? Worker() : null,
